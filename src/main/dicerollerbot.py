46469366b9db6a06c4437561a5937cf73b67060b
Charlie Hamel <charleshamel73@yahoo.com>
2016-06-15 22:48:22 -0500

    Added Slackbot Client

--- src/main/dicerollerbot.py
+++ src/main/dicerollerbot.py
@@ -1,7 +1,23 @@
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
+import re
+
+from src.main.Parser import Parser
+
+
 def main():
     bot = Bot()
     bot.run()
 
+
 if __name__ == "__main__":
     main()
+
+@respond_to('roll (.*)')
+def parse(message,roll_string):
+    Parser().parse(roll_string)
+    message.reply('I can understand hi or HI!')
+
+@respond_to('I love you')
+def love(message):
+    message.reply('I love you too!')
---
+++ src/main/slackbot_settings.py
@@ -0,0 +1,6 @@
+API_TOKEN = "xoxb-51369672387-wipnUtaAxhQDD1vspXHmn2RS"
+DEFAULT_REPLY = "NO RESPONSE"
+PLUGINS = [
+    'slackbot.plugins',
+    'mybot.plugins',
+]
